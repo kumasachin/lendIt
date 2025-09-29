f317ebd3eb2401e02cbcb1cc32445d095acbce3a
skumar <kumasachin@gmail.com>
2025-09-29 09:56:28 +0100

    analytics implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import LoanCalculator from "./components/LoanCalculator";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
-  return <LoanCalculator />;
+  return (
+    <>
+      <LoanCalculator />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
